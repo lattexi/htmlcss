76dd7541cc49f953bafc72c3ceace16e7b0467cd
lattexi <laurikarhu@outlook.com>
2024-09-17 15:56:20 +0300

    Initial

---
+++ tehtävä.js
@@ -0,0 +1,6 @@
+const loginButton = document.getElementById('login');
+const dialog = document.getElementById('dialog');
+
+loginButton.addEventListener('click', () => {
+    dialog.showModal();
+});
